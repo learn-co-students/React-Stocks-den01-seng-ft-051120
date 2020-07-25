0e130f5f7e21f0a981df2a7939a3d6a186ad9bcc
Bryce Kennedy <btken88@gmail.com>
2020-07-25 14:06:28 -0600

    Done.

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -4,24 +4,101 @@ import PortfolioContainer from './PortfolioContainer'
 import SearchBar from '../components/SearchBar'
 
 class MainContainer extends Component {
+  state = {
+    stocks: [],
+    portfolio: [],
+    filter: ""
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/stocks')
+      .then(response => response.json())
+      .then(stocks => this.setState({ stocks }))
+  }
+
+  addStock = (stock) => {
+    const updatedStocks = this.state.stocks.filter(oldStock => {
+      return oldStock !== stock
+    })
+    this.setState({
+      stocks: updatedStocks,
+      portfolio: [...this.state.portfolio, stock]
+    })
+  }
+
+  removeStock = (stock) => {
+    const updatedStocks = this.state.portfolio.filter(oldStock => {
+      return oldStock !== stock
+    })
+
+    this.setState({
+      stocks: [...this.state.stocks, stock],
+      portfolio: updatedStocks
+    })
+  }
+
+  sortStocksAlpha = e => {
+    console.log('sort')
+    const alpha = this.state.stocks.sort((a, b) => {
+      if (a.ticker < b.ticker) {
+        return -1
+      }
+      if (a.ticker > b.ticker) {
+        return 1
+      }
+      return 0
+    })
+    this.setState({
+      ...this.state,
+      stocks: alpha
+    })
+  }
+
+  sortStocksPrice = e => {
+    const price = this.state.stocks.sort((a, b) => {
+      return a.price - b.price
+    })
+    this.setState({
+      ...this.state,
+      stocks: price
+    })
+  }
+
+  filterStocks = e => {
+    const filterString = e.target.value
+    this.setState({
+      ...this.state,
+      filter: filterString
+    })
+  }
 
   render() {
+    const { stocks, portfolio, filter } = this.state
+
+    const filteredStocks = (filter) => {
+      if (filter !== "") {
+        return stocks.filter(stock => {
+          return stock.type === filter
+        })
+      }
+      return stocks
+    }
     return (
       <div>
-        <SearchBar/>
+        <SearchBar alpha={this.sortStocksAlpha} price={this.sortStocksPrice} filter={this.filterStocks} />
 
-          <div className="row">
-            <div className="col-8">
+        <div className="row">
+          <div className="col-8">
 
-              <StockContainer/>
+            <StockContainer stocks={filteredStocks(filter)} addStock={this.addStock} />
 
-            </div>
-            <div className="col-4">
-
-              <PortfolioContainer/>
-
-            </div>
           </div>
+          <div className="col-4">
+
+            <PortfolioContainer stocks={portfolio} removeStock={this.removeStock} />
+
+          </div>
+        </div>
       </div>
     );
   }
--- src/containers/StockContainer.js
+++ src/containers/StockContainer.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 import Stock from '../components/Stock'
 
 class StockContainer extends Component {
-
   render() {
+    const { stocks, addStock } = this.props
+    const stocksList = stocks.map(stock => {
+      return <Stock key={stock.id} stock={stock} updatePortfolio={addStock} />
+    })
     return (
       <div>
         <h2>Stocks</h2>
-        {
-          //render the list of stocks here
-        }
+        {stocks.length ? stocksList : null}
       </div>
     );
   }
